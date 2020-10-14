3231da989f7a83458ce3b1e79df163647729338d
CallMeDKay <s3812649@rmit.edu.vn>
2020-10-14 22:29:58 +0700

    initialize navbar 14.10

---
+++ src/navigation/NavigationBar.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link, NavLink, BrowserRouter } from "react-router-dom";
+import "./NavigationBar.css";
+
+function NavigationBar() {
+	return (
+		<nav className="flexbox">
+			<BrowserRouter>
+				<div className="flexbox-left">
+					<NavLink className="navtags-logo" activeClassName="logo" exact to="/">
+						TenderMate
+					</NavLink>
+					<ul className="topNav">
+						<li>
+							<NavLink className="navtags" activeClassName="selected" to="/features">
+								Features
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="navtags" activeClassName="selected" to="/explore">
+								Explore
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="navtags" activeClassName="selected" to="/pricing">
+								Pricing
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="navtags" activeClassName="selected" to="/about">
+								About Us
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+				<div className="flexbox-right">
+					<NavLink className="navtags push" activeClassName="selected" to="/signin">
+						Sign in
+					</NavLink>
+					<NavLink className="navtags push" activeClassName="selected" to="/signup">
+						Sign up
+					</NavLink>
+				</div>
+			</BrowserRouter>
+		</nav>
+	);
+}
+
+export default NavigationBar;
