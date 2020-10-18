2f8de38733425a076cf2f798d29dbf78862df9eb
hungnguyen <hungthezorba@gmail.com>
2020-10-18 21:43:26 +0700

    Added Interesting-Choosing page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Login from './loginForm/Login'
 import Register from './registerForm/Register'
 import './Form.css'
 import NavigationBar from './navigation/NavigationBar.js';
-
+import FieldDiv from './interestingField/FieldDiv'
+import Interesting from './interestingField/Interesting'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
+      <Interesting/>
+
     </div>
   );
 }
--- src/interestingField/Field.js
+++ src/interestingField/Field.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import './Field.css'
+import Box from '@material-ui/core/Box'
+
 export default class Field extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            isChosen : false
-        }
-        this.hasChosen = this.hasChosen.bind(this)
     }
 
-    hasChosen(e) {
-        e.preventDefault();
-        this.setState({isChosen : !this.state.isChosen})
+    handleChange(e) {
+        this.props.isChosen = !this.props.isChosen
     }
+
 
     render() {
         return (
-            <div>
-                <button type="button" className={"field-button " + (this.state.isChosen ? 'active' : '')} onClick={this.hasChosen}>
+            <Box flex="0 0 calc(16.66% - 20px)" ml={0.75} mr={0.75} mb={7}> 
+                <button type="button" className={"field-button " + (this.props.isChosen ? 'active' : '')} onClick={(e) => this.props.onClick(this.props.index)} >
                     {this.props.name}
                 </button>
-            </div>
+            </Box>
         )
     }
     
