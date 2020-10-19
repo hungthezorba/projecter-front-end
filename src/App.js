e85b61e3b209b53e79036837e8ab51fcb7c362f7
CallMeDKay <s3812649@rmit.edu.vn>
2020-10-19 22:29:41 +0700

    structured layout 19.10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import './App.css';
-import Login from './loginForm/Login'
-import Register from './registerForm/Register'
-import './Form.css'
-import NavigationBar from './navigation/NavigationBar.js';
-import Footer from './navigation/Footer.js';
-
+import React from "react";
+import "./App.css";
+import Login from "./loginForm/Login";
+import Register from "./registerForm/Register";
+import "./Form.css";
+import NavigationBar from "./navigation/NavigationBar.js";
+import Footer from "./navigation/Footer.js";
+import eFeatures from "./page/eFeatures.js";
+import eHomePage from "./page/eHomepage.js"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <NavigationBar></NavigationBar>
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<div className="wrapper">
+					<Switch>
+						<Route path="/" exact component = {eHomePage}/>
+						<Route path="/features" component = {eFeatures}/>
+					</Switch>
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/page/eHomepage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NavigationBar from "../navigation/NavigationBar.js";
+import Footer from "../navigation/Footer.js";
+
+export default class eHomePage extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	// methods here
+
+	render() {
+		return (
+			<div>
+				<NavigationBar></NavigationBar>
+				<div className="main-content">Homepage loading 99%</div>
+				<Footer></Footer>
+			</div>
+		);
+	}
+}
