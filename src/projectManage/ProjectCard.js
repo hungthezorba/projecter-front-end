5e5fbe39fd0ee42e16b8c307fef7f14ebb0e888d
hungnguyen <hungthezorba@gmail.com>
2020-10-20 21:19:10 +0700

    Reworked on project's card image

--- src/projectManage/ProjectCard.js
+++ src/projectManage/ProjectCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components'
+import Box from '@material-ui/core/Box'
 import './ProjectManage.css'
 
 const Content = styled.div`
@@ -23,53 +24,57 @@ const Setting = styled.div`
     }
 `
 
+const ImageContainer = (props) => (
+    <Box>
+        <img style={{ borderRadius: "10px" }} width="340" height="280" src={props.source} />
+    </Box>
+)
+
 
 export default class ProjectCard extends React.Component {
 
     render() {
         return (
-            <Card style={{minWidth: '100%', width: '80vw', borderRadius: '10px'}}>
+            <Card style={{ minWidth: '100%', width: '80vw', borderRadius: '10px' }}>
                 <Content>
-                <div>
-                    <img component="img" alt="projectThumbnail" width="341" height="280" src="https://media.monsterindia.com/cmsimages/1509963788.jpg" />
-                </div>
-                <div style={{display: 'flex', flexDirection: 'column', padding: '20px', width: '40%'}}>
-                    <div>
-                        <Typography>
-                            Ocean Freedom
-                        </Typography>
-                    </div>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                    <div style={{display: 'flex', alignItems: 'center', marginLeft: '10px'}}>
-                        <div style={{display: 'flex'}}>
-                            <div>
-                                <img style={{borderRadius: '50%', width: '50px'}} src="https://www.w3schools.com/howto/img_avatar.png"/>
+                    <ImageContainer source="https://media.monsterindia.com/cmsimages/1509963788.jpg" />
+                    <div style={{ display: 'flex', flexDirection: 'column', padding: '20px', width: '40%', justifyContent: 'space-between' }}>
+                        <div>
+                            <Typography component="h5" variant="h4">
+                                Ocean Freedom
+                            </Typography>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
+                            <div style={{ display: 'flex' }}>
+                                <div>
+                                    <img style={{ borderRadius: '50%', width: '50px' }} src="https://www.w3schools.com/howto/img_avatar.png" />
+                                </div>
+                                <div>
+                                    <img style={{ borderRadius: '50%', width: '50px' }} src="https://www.w3schools.com/howto/img_avatar.png" />
+                                </div>
+                                <div>
+                                    <img style={{ borderRadius: '50%', width: '50px' }} src="https://www.w3schools.com/howto/img_avatar.png" />
+                                </div>
+                                <div>
+                                    <img style={{ borderRadius: '50%', width: '50px' }} src="https://www.w3schools.com/howto/img_avatar.png" />
+                                </div>
                             </div>
-                            <div>
-                                <img style={{borderRadius: '50%', width: '50px'}} src="https://www.w3schools.com/howto/img_avatar.png"/>
-                            </div>
-                            <div>
-                                <img style={{borderRadius: '50%', width: '50px'}} src="https://www.w3schools.com/howto/img_avatar.png"/>
-                            </div>
-                            <div>
-                                <img style={{borderRadius: '50%', width: '50px'}} src="https://www.w3schools.com/howto/img_avatar.png"/>
+                            <div style={{ padding: '0 20px' }}>
+                                <button className="add-member-button">Add</button>
                             </div>
                         </div>
-                        <div style={{marginLeft: '10px'}}>
-                            <button className="add-member-button">Add</button>
+                    </div>
+                    <Setting>
+                        <div className="setting-icon">
+                            <i class="fas fa-eye-slash"></i>
                         </div>
-                    </div>
-                </div>
-                <Setting>
-                    <div className="setting-icon">
-                        <i class="fas fa-eye-slash"></i>
-                    </div>
-                    <div className="setting-icon">
-                        <i class="fas fa-cog"></i>
-                    </div>
-                </Setting>
+                        <div className="setting-icon">
+                            <i class="fas fa-cog"></i>
+                        </div>
+                    </Setting>
                 </Content>
             </Card>
         )
