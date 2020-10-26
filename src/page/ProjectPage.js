23bbce000467ab67579acc4dcb4a031971284ce8
hungnguyen <hungthezorba@gmail.com>
2020-10-26 21:27:57 +0700

    Added dummy state

--- src/page/ProjectPage.js
+++ src/page/ProjectPage.js
@@ -2,18 +2,50 @@ import React from 'react'
 import ProjectCard from '../projectManage/ProjectCard'
 import styled from 'styled-components'
 import Note from '../projectManage/Note'
+
 const ProjectManageWrapper = styled.div`
     padding: 5em 7em 5em 7em;
 `
 
-const ProjectPage = () => (
-    <ProjectManageWrapper>
-        <ProjectCard/>
-        <div style={{height: 'auto', width: '80vw', padding: '20px', position: 'relative'}}>
-            <Note/>
-            <Note/>
-        </div>
-    </ProjectManageWrapper>
-)
+export default class ProjectPage extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            notes: [
+                {
+                    id: '1',
+                    title: 'Protect the fish!',
+                    items: [
+                        { id: '1', content: 'hello', date: '18/09/2020', done: false },
+                        { id: '2', content: 'world', date: '15/10/2020', done: false }
+                    ],
+                    x: '0',
+                    y: '0'
+                },
+                {
+                    id: '2',
+                    title: 'Search the warehouse',
+                    items: [
+                        { id: '3', content: 'hello', date: '18/09/2020', done: false },
+                        { id: '4', content: 'world', date: '15/10/2020', done: true }
+                    ],
+                    x: '0',
+                    y: '0'
+                }
+            ]
+        }
+    }
 
-export default ProjectPage
+    render() {
+        return (
+            <ProjectManageWrapper>
+                <ProjectCard />
+                <div style={{ height: 'auto', width: '100%', margin: '25px auto', position: 'relative' }}>
+                    {this.state.notes.map(note => (
+                        <Note data={note} />
+                    ))}
+                </div>
+            </ProjectManageWrapper>
+        )
+    }
+}
--- src/projectManage/Note.js
+++ src/projectManage/Note.js
@@ -4,73 +4,36 @@ import Draggable from 'react-draggable'
 
 export default class Note extends React.Component {
 
-    state = {
-        activeDrags: 0,
-        deltaPosition: {
-            x: 0, y:0
-        },
-    }
-
-    onStart = () => {
-        this.setState({activeDrags: ++this.state.activeDrags})
+    constructor(props) {
+        super(props)
     }
 
     render() {
         return (
             <Draggable bounds="parent">
-                <div style={{ fontFamily: 'Quicksand', maxWidth: '500px', backgroundColor: '#F8F8F8', padding: '15px 35px 15px 35px', borderRadius: '10px' }}>
+                <div style={{marginTop: '25px', fontFamily: 'Quicksand', maxWidth: '500px', backgroundColor: '#F8F8F8', padding: '15px 35px 15px 35px', borderRadius: '10px' }}>
                     <div>
-                        <h1 style={{ fontSize: '2.5em' }}>Protect the fish!</h1>
+                        <h1 style={{ fontSize: '2.5em' }}>{this.props.data.title}</h1>
                     </div>
                     <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <div style={{ display: 'flex', position: 'relative' }}>
-                            <div>
-                                <div>
-                                    <p style={{ color: '#A6A6A6' }}>18/09/2020</p>
+                        {this.props.data.items.map(item => (
+                            <div style={{ display: 'flex', position: 'relative' }}>
+                                <div style={{ maxWidth: '30em' }}>
+                                    <div>
+                                        <p style={{ color: '#A6A6A6' }}>{item.date}</p>
+                                    </div>
+                                    <div>
+                                        <p>{item.content}</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <p>Lorem ispum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="check-box-custom" style={{ position: 'absolute', right: '0', top: '30px' }}>
-                                <label class="check-label" for="task-1">
-                                    <input type="checkbox" id="task-1" />
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                        </div>
-                        <div style={{ display: 'flex', position: 'relative' }}>
-                            <div>
-                                <div>
-                                    <p style={{ color: '#A6A6A6' }}>18/09/2020</p>
-                                </div>
-                                <div>
-                                    <p>Lorem ispum dolor sit amet, consectetur adipiscing elit.</p>
+                                <div class="check-box-custom" style={{ position: 'absolute', right: '0', top: '30px' }}>
+                                    <label class="check-label" for={"task-" + item.id}>
+                                        <input type="checkbox" id={"task-" + item.id} defaultChecked={item.done} />
+                                        <span class="checkmark"></span>
+                                    </label>
                                 </div>
                             </div>
-                            <div class="check-box-custom" style={{ position: 'absolute', right: '0', top: '30px' }}>
-                                <label class="check-label" for="task-1">
-                                    <input type="checkbox" id="task-1" />
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                        </div>
-                        <div style={{ display: 'flex', position: 'relative' }}>
-                            <div style={{ maxWidth: '30em' }}>
-                                <div>
-                                    <p style={{ color: '#A6A6A6' }}>18/09/2020</p>
-                                </div>
-                                <div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo mauris arcu, sit amet volutpat lacus ornare sit amet. Suspendisse eget neque a arcu ultricies viverra. Nulla facilisi. Nulla facilisi.</p>
-                                </div>
-                            </div>
-                            <div class="check-box-custom" style={{ position: 'absolute', right: '0', top: '30px' }}>
-                                <label class="check-label" for="task-1">
-                                    <input type="checkbox" id="task-1" />
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </Draggable>
