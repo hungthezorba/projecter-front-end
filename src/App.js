c20368acb580467d1e18bac0920a2977d91b937f
CallMeDKay <s3812649@rmit.edu.vn>
2020-10-29 15:31:17 +0700

    Fix dupplicated import statements

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Register from "./page/Register";
 import "./Form.css";
 import NavigationBar from "./navigation/NavigationBar.js";
 import Footer from "./navigation/Footer.js";
-import Interesting from "./interestingField/Interesting"
 import UserCard from "./userDisplay/UserCard.js";
 
 /* Import pages */
@@ -16,7 +15,6 @@ import eFeatures from "./page/eFeatures.js";
 import eHomePage from "./page/eHomepage.js"
 import UserProfile from "./page/UserProfile";
 import Interesting from "./page/Interesting"
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import ProjectPage from "./page/ProjectPage";
 
 function App() {
