1a8534348025077d247d2888576acaa4b457918f
hungnguyen <hungthezorba@gmail.com>
2020-10-30 08:25:10 +0700

    Add loading animation

--- src/projectManage/Note.js
+++ src/projectManage/Note.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Draggable from 'react-draggable'
+import Skeleton from 'react-loading-skeleton'
 
 
 export default class Note extends React.Component {
@@ -15,44 +16,50 @@ export default class Note extends React.Component {
     }
 
     handleDrag = (e, ui) => {
-        const {x, y} = this.props.data;
+        const { x, y } = this.props.data;
         this.setState({
             deltaPosition: {
                 x: x + ui.deltaX,
                 y: y + ui.deltaY
-            },    
+            },
         })
         this.props.handler(this.props.data.id, this.state.deltaPosition.x, this.state.deltaPosition.y)
     }
 
     render() {
         return (
-            <Draggable defaultPosition={{x: this.state.deltaPosition.x, y: this.state.deltaPosition.y}} onStop={this.props.onStop} onDrag={this.handleDrag} bounds="parent">
-                <div style={{marginTop: '25px', fontFamily: 'Quicksand', maxWidth: '500px', backgroundColor: '#F8F8F8', padding: '15px 35px 15px 35px', borderRadius: '10px' }}>
-                    <div>
-                        <h1 style={{ fontSize: '2em' }}>{this.props.data.title}</h1>
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        {this.props.data.items.map(item => (
-                            <div style={{ display: 'flex', position: 'relative' }}>
-                                <div style={{ maxWidth: '30em' }}>
-                                    <div>
-                                        <p style={{ color: '#A6A6A6' }}>{item.date}</p>
-                                    </div>
-                                    <div>
-                                        <p>{item.content}</p>
-                                    </div>
-                                </div>
-                                <div class="check-box-custom" style={{ position: 'absolute', right: '0', top: '30px' }}>
-                                    <label class="check-label" for={"task-" + item.id}>
-                                        <input type="checkbox" id={"task-" + item.id} defaultChecked={item.done} />
-                                        <span class="checkmark"></span>
-                                    </label>
-                                </div>
+            <Draggable defaultPosition={{ x: this.state.deltaPosition.x, y: this.state.deltaPosition.y }} onStop={this.props.onStop} onDrag={this.handleDrag} bounds="parent">
+                <div style={{ marginTop: '25px', fontFamily: 'Quicksand', maxWidth: '500px', backgroundColor: '#F8F8F8', padding: '15px 35px 15px 35px', borderRadius: '10px' }}>
+                    {this.props.data.items ?
+                        <div>
+                            <div>
+                                <h1 style={{ fontSize: '2em' }}>{this.props.data.title}</h1>
                             </div>
-                        ))}
-                    </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                {this.props.data.items.map(item => (
+                                    <div style={{ display: 'flex', position: 'relative' }}>
+                                        <div style={{ maxWidth: '30em' }}>
+                                            <div>
+                                                <p style={{ color: '#A6A6A6' }}>{item.date}</p>
+                                            </div>
+                                            <div>
+                                                <p>{item.content}</p>
+                                            </div>
+                                        </div>
+                                        <div class="check-box-custom" style={{ position: 'absolute', right: '0', top: '30px' }}>
+                                            <label class="check-label" for={"task-" + item.id}>
+                                                <input type="checkbox" id={"task-" + item.id} defaultChecked={item.done} />
+                                                <span class="checkmark"></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div> :
+                        <Skeleton />
+                    }
                 </div>
+
             </Draggable>
         )
     }
